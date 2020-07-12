660b8918a24c06ec849ba8081f525fa593fdba98
Manav Misra <manavm@visionwebsoft.com>
2020-07-12 13:45:07 -0500

    :fire: Remove unused rep routes

--- api/repository.js
+++ api/repository.js
@@ -3,19 +3,7 @@ export default ($axios) => (resource) => ({
     return $axios.$get(`http://localhost:5000/${resource}`)
   },
 
-  // create(payload) {
-  //   return $axios.$post(`/posts`, payload)
-  // },
-
   show(id) {
     return $axios.$get(`http://localhost:5000/${resource}/${id}`)
   },
-
-  // update(payload, id) {
-  //   return $axios.$put(`/posts/${id}`, payload)
-  // },
-
-  // delete(id) {
-  //   return $axios.$delete(`/posts/${id}`)
-  // },
 })
